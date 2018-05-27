86ea321c920954d0acf59639820bca0ec748ba36
jafahulo <max.j.wells@gmail.com>
2018-05-27 13:41:41 -0500

    Complete rewrite of Host and Device0 files

--- Device0_Necklace/Device0_Necklace.ino
+++ Device0_Necklace/Device0_Necklace.ino
@@ -1,63 +1,67 @@
-/*
-This sketch demonstrates how to send data from a Device
-to a Host in a Gazell network.
-
-When Button A on the Device is pressed and released,
-the green led on the host will toggle.
-*/
-
 #include <RFduinoGZLL.h>
-
-device_t role = DEVICE0;
 
 int red_led = 2;
 int green_led = 3;
 int blue_led = 4;
-double initialIntensity = .75;
-double intensity = .75;
+bool isConnected = false;
+double setIntensity = .45;
+double intensity = 0.0;
 int red = 127;
 int green = 255;
 int blue = 212;
 int timer = millis();
-void setup()
-{
+device_t role = DEVICE0;
+
+void setup() {
+  // put your setup code here, to run once:
   pinMode(red_led, OUTPUT);
   pinMode(green_led, OUTPUT);
   pinMode(blue_led, OUTPUT);
+  Serial.begin(9600);
   // start the GZLL stack
   RFduinoGZLL.txPowerLevel = 0;
   RFduinoGZLL.begin(role);
   RFduinoGZLL.sendToHost(1);
 }
 
-void rgb(){
-  analogWrite(red_led, red*intensity);
-  analogWrite(green_led, green*intensity);
-  analogWrite(blue_led, blue*intensity);
-}
-
-void rgbKill(){
-  for(intensity; intensity>0; intensity-=.01){
-    rgb();
-    delay(100);
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (isConnected) {
+    increaseIntensity();
+  } else {
+    decreaseIntensity();
   }
-  digitalWrite(red_led, LOW);
-  digitalWrite(green_led, LOW);
-  digitalWrite(blue_led, LOW);
+  if (millis() - timer > 3000) {
+    isConnected = false;
+  }
+  writeLED();
+  RFduinoGZLL.sendToHost(1);
+  delay(50);
 }
 
-void loop()
-{
-  if(millis()-timer > 3000)
-    rgbKill();
+void increaseIntensity(){
+  intensity += .01;
+  if(intensity>setIntensity){
+    intensity=setIntensity;
+  }
+}
 
+void decreaseIntensity(){
+  intensity -= .01;
+  if(intensity<0){
+    intensity=0;
+  }
+}
+
+void writeLED(){
+  analogWrite(red_led, red * intensity);
+  analogWrite(blue_led, blue * intensity);
+  analogWrite(green_led, green * intensity);
 }
 
 void RFduinoGZLL_onReceive(device_t device, int rssi, char *data, int len)
 {
-  intensity = initialIntensity;
-  rgb();
+  isConnected = true;
   timer = millis();
-//  delay(2000);
-  RFduinoGZLL.sendToHost(NULL);
 }
+
